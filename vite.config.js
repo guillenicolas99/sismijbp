cb28dfaf66263f963d3149953801242d7ed3ff0b
Guillermo Nicolas <guillenicolas99@gmail.com>
2025-05-05 00:08:07 -0600

    re_change_vite_config

--- vite.config.js
+++ vite.config.js
@@ -1,21 +1,3 @@
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         })],
-//     build: {
-//         outDir: 'public/build',
-//         manifest: true,
-//         rollupOptions: {
-//             input: ['resources/js/app.js']
-//         }
-//     }
-// });
-
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
 
@@ -26,5 +8,28 @@ export default defineConfig({
             refresh: true,
         }),
     ],
+    build: {
+        outDir: 'public/build',
+        manifest: true,
+        rollupOptions: {
+            input: [
+                'resources/css/app.css',
+                'resources/js/app.js'
+            ]
+        }
+    }
 });
+
+
+// import { defineConfig } from 'vite';
+// import laravel from 'laravel-vite-plugin';
+
+// export default defineConfig({
+//     plugins: [
+//         laravel({
+//             input: ['resources/css/app.css', 'resources/js/app.js'],
+//             refresh: true,
+//         }),
+//     ],
+// });
 
