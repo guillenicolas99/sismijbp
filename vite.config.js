288fa71489f7c3005fb7db19d8b3638300d0fa85
Guillermo Nicolas <guillenicolas99@gmail.com>
2025-05-05 00:14:55 -0600

    resotre_vite_config

--- vite.config.js
+++ vite.config.js
@@ -8,28 +8,5 @@ export default defineConfig({
             refresh: true,
         }),
     ],
-    build: {
-        outDir: 'public/build',
-        manifest: true,
-        rollupOptions: {
-            input: [
-                'resources/css/app.css',
-                'resources/js/app.js'
-            ]
-        }
-    }
 });
-
-
-// import { defineConfig } from 'vite';
-// import laravel from 'laravel-vite-plugin';
-
-// export default defineConfig({
-//     plugins: [
-//         laravel({
-//             input: ['resources/css/app.css', 'resources/js/app.js'],
-//             refresh: true,
-//         }),
-//     ],
-// });
 
